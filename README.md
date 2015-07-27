aada2539d26c6c15bb4b4e5b6c7c9ffad4774b23
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-07-27 21:03:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# escape-json for Node.js
+
+Escape nested double quotes and unescape apostrophes within JSON string.
+
+[![NPM Version][npm-image]][npm-url]
+[![NPM Downloads][downloads-image]][downloads-url]
+[![License][license]][license-url]
+[![Travis Build][travis-image]][travis-url]
+
+## Installation
+
+`npm install escape-json-node`
+
+## Usage Example
+
+```javascript
+var escapeJSON = require("escape-json-node");
+
+var JSONString = "{\"quoteText\": \"'Acceptance says, \"True, this is " +
+"my situation at the moment. I\'ll look unblinkingly at the reality of it. But I will also open my " +
+"hands to accept willingly whatever a loving Father sends me.\"\"}";
+
+JSONString = escapeJSON(JSONString);
+
+console.log(JSONString);
+```
+
+***
+
+```json
+{"quoteText": "'Acceptance says, \"True, this is my situation at the moment. 
+I'll look unblinkingly at the reality of it. But I will also open my 
+hands to accept willingly whatever a loving Father sends me.\""}
+```
+
+## Tests
+
+To run the test suite, first install the dependencies, then run `npm test`:
+
+```bash
+$ npm install
+$ npm test
+```
+
+## License
+
+Distributed under the [MIT License](LICENSE).
+
+[npm-image]: https://img.shields.io/npm/v/escape-json-node.svg
+[npm-url]: https://npmjs.org/package/escape-json-node
+[downloads-image]: https://img.shields.io/npm/dm/escape-json-node.svg
+[downloads-url]: https://npmjs.org/package/escape-json-node
+[license]: https://img.shields.io/npm/l/escape-json-node.svg
+[license-url]: https://github.com/AnatoliyGatt/escape-json-node/blob/master/LICENSE
+[travis-image]: https://img.shields.io/travis/AnatoliyGatt/escape-json-node/master.svg
+[travis-url]: https://travis-ci.org/AnatoliyGatt/escape-json-node
---
+++ test/escape-json.js
@@ -0,0 +1,70 @@
+var assert = require("assert");
+var escapeJSON = require("../lib/escape-json");
+
+describe("escape-json", function() {
+    describe("init", function() {
+        it("should not modify JSON string which is not containing any apostrophes or double quotes", function() {
+            var originalJSONString = "{\"quoteText\": \"How far that little candle throws its beams! So shines a " +
+                "good deed in a naughty world.\"}";
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"How far that little candle throws its beams! So shines a " +
+            "good deed in a naughty world.\"}");
+            assert.equal(escapedJSONString, originalJSONString, "JSON string should not be modified");
+        });
+
+        it("should unescape apostrophe within JSON string", function() {
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"Interesting when rain falls " +
+            "somewhere only to recognize that it isn\'t falling some place else.\"}");
+            var expectedEscapedJSONString = "{\"quoteText\": \"Interesting when rain falls somewhere only to " +
+                "recognize that it isn\'t falling some place else.\"}";
+            assert.equal(escapedJSONString, expectedEscapedJSONString, "apostrophe should be unescaped within JSON string");
+        });
+
+        it("should unescape apostrophes withing JSON string", function() {
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"Don\'t turn away from possible futures before you\'re certain you " +
+            "don\'t have anything to learn from them.\"}");
+            var expectedEscapedJSONString = "{\"quoteText\": \"Don't turn away from possible futures before you're certain you " +
+                "don't have anything to learn from them.\"}";
+            assert.equal(escapedJSONString, expectedEscapedJSONString, "apostrophes should be unescaped within JSON string");
+        });
+
+        it("should escape nested double quotes within JSON string", function() {
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"If you have made mistakes, there is " +
+            "always another chance for you. You may have a fresh start any moment you choose, for this thing we " +
+            "call \"failure\" is not the falling down, but the staying down.\"}");
+            var expectedEscapedJSONString = "{\"quoteText\": \"If you have made mistakes, there is " +
+                "always another chance for you. You may have a fresh start any moment you choose, for this thing we " +
+                "call \\\"failure\\\" is not the falling down, but the staying down.\"}";
+            assert.equal(escapedJSONString, expectedEscapedJSONString, "nested double quotes should be escaped within JSON string");
+        });
+
+        it("should escape nested double quotes and unescape apostrophe within JSON string", function() {
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"'Acceptance says, \"True, this is " +
+            "my situation at the moment. I\'ll look unblinkingly at the reality of it. But I will also open my " +
+            "hands to accept willingly whatever a loving Father sends me.\"\"}");
+            var expectedEscapedJSONString = "{\"quoteText\": \"'Acceptance says, \\\"True, this is " +
+                "my situation at the moment. I'll look unblinkingly at the reality of it. But I will also open my " +
+                "hands to accept willingly whatever a loving Father sends me.\\\"\"}";
+            assert.equal(escapedJSONString, expectedEscapedJSONString, "nested double quotes should be escaped and apostrophe should be unescaped within JSON string");
+        });
+
+        it("should escape nested double quotes and unescape apostrophes within JSON string", function() {
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"'Acceptance says, \"True, this is " +
+            "my situation at the moment. I\'ll look unblinkingly at the reality of it. But I\'ll also open my " +
+            "hands to accept willingly whatever a loving Father sends me.\"\"}");
+            var expectedEscapedJSONString = "{\"quoteText\": \"'Acceptance says, \\\"True, this is " +
+                "my situation at the moment. I'll look unblinkingly at the reality of it. But I'll also open my " +
+                "hands to accept willingly whatever a loving Father sends me.\\\"\"}";
+            assert.equal(escapedJSONString, expectedEscapedJSONString, "nested double quotes should be escaped and apostrophes should be unescaped within JSON string");
+        });
+
+        it("should escape multiple nested double quotes and unescape apostrophes within JSON string", function() {
+            var escapedJSONString = escapeJSON("{\"quoteText\": \"'Acceptance says, \"True, this is " +
+            "my situation at the moment. I\'ll look \"unblinkingly\" at the reality of it. But I\'ll also open my " +
+            "hands to accept willingly whatever a loving Father sends me.\"\"}");
+            var expectedEscapedJSONString = "{\"quoteText\": \"'Acceptance says, \\\"True, this is " +
+                "my situation at the moment. I'll look \\\"unblinkingly\\\" at the reality of it. But I'll also open my " +
+                "hands to accept willingly whatever a loving Father sends me.\\\"\"}";
+            assert.equal(escapedJSONString, expectedEscapedJSONString, "multiple nested double quotes should be escaped and apostrophes should be unescaped within JSON string");
+        });
+    });
+});
