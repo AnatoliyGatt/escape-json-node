9250054b019909d36e613f7bc3916ecb52ffc27d
Anatoliy Gatt <anatoliy.gatt@aol.com>
2017-09-07 16:20:25 +0200

    2.0.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @module index
  * @description Entry point for escape-json-node module.
- * @version 1.0.8
+ * @version 2.0.0
  * @author Anatoliy Gatt [anatoliy.gatt@aol.com]
  * @copyright Copyright (c) 2015-2017 Anatoliy Gatt
  * @license MIT
